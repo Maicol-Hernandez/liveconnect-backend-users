f988cbe0b9acd296fc1ad03ade2ff3ab092a359a
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-06 22:13:29 -0500

    Allows nullable Exception in constructors
    Improves type hinting by allowing the `previous` Exception parameter in several exception classes to be nullable.  This enhances code clarity and flexibility.

--- app/Exceptions/HttpException.php
+++ app/Exceptions/HttpException.php
@@ -6,10 +6,11 @@ use Exception;
 
 class HttpException extends Exception
 {
-
-
-    public function __construct(string $message  = 'Internal Server Error', int $status_http_code = 500, Exception  $previous  = null)
-    {
+    public function __construct(
+        string $message = 'Internal Server Error',
+        int $status_http_code = 500,
+        ?Exception $previous = null
+    ) {
         parent::__construct($message, $status_http_code, $previous);
     }
 }
