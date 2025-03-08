bea9c40b45356a94a260e648c71b8d9d4359ca43
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-07 19:58:31 -0500

    Updates database connection configuration
    Renames database environment variables for consistency and adds charset to database connection string.  Updates docker-compose file to reflect these changes.

--- app/Database/Connection.php
+++ app/Database/Connection.php
@@ -17,8 +17,8 @@ class Connection
     {
         try {
             $this->connection = new PDO(
-                "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}",
-                $_ENV['DB_USER'],
+                "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8",
+                $_ENV['DB_USERNAME'],
                 $_ENV['DB_PASSWORD']
             );
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
