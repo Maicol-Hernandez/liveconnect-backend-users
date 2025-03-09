d403d6beef2eb0b00a072dc058987c5d50e4bb66
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-08 21:32:26 -0500

    Sets default timezone to America/Bogota
    Configures the application to use the America/Bogota timezone for all date and time operations.

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS, PUT, DELETE');
 header('Allow: GET, POST, PATCH, OPTIONS, PUT, DELETE');
+date_default_timezone_set('America/Bogota');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/helpers/helpers.php';
